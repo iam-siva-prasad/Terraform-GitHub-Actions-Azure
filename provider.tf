d231fd77f409ad9b46b1b2d074fded261482609e
SIVA PRASAD <55012125+iam-siva-prasad@users.noreply.github.com>
2026-01-04 11:51:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 
+
 terraform {
   required_version = ">= 1.4.6"
   required_providers {
@@ -6,26 +7,27 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
-  # }
-  # backend "azurerm" {
-  #   resource_group_name  = "1-3f818d04-playground-sandbox"
-  #   storage_account_name = "sivaprasadxyz"
-  #   container_name       = "new"
-  #   key                  = "prod.terraform.tfstate"
-  # }
-}
+   }
+   backend "azurerm" {
+     resource_group_name  = "1-3f818d04-playground-sandbox"
+     storage_account_name = "sivaprasadxyz"
+     container_name       = "jil"
+     key                  = "siva.terraform.tfstate"
+     use_azuread_auth     = true
+   }
 }
 
-provider "azurerm" {
-  features {}
-  # If you’re using Service Principal env vars:
-  # (Optional — you can remove these if you set ARM_* env vars in the pipeline)
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  # Temporarily disable auto-registration to avoid 403s at init time:
-  resource_provider_registrations = "none"
-}
+
+# provider "azurerm" {
+#   features {}
+#    #If you’re using Service Principal env vars:
+#    #(Optional — you can remove these if you set ARM_* env vars in the pipeline)
+#   subscription_id = var.subscription_id
+#   tenant_id       = var.tenant_id
+#   client_id       = var.client_id
+#   client_secret   = var.client_secret
+#    #Temporarily disable auto-registration to avoid 403s at init time:
+#   resource_provider_registrations = "none"
+# }
 
 
