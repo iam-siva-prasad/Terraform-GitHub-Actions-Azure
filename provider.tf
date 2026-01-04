b7662eb1d277f545bf33f4809a9b1c7015abea39
SIVA PRASAD <55012125+iam-siva-prasad@users.noreply.github.com>
2026-01-04 12:19:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -23,16 +23,3 @@ provider "azurerm" {
   resource_provider_registrations = "none"
 }
 
-# provider "azurerm" {
-#   features {}
-#    #If you’re using Service Principal env vars:
-#    #(Optional — you can remove these if you set ARM_* env vars in the pipeline)
-#   subscription_id = var.subscription_id
-#   tenant_id       = var.tenant_id
-#   client_id       = var.client_id
-#   client_secret   = var.client_secret
-#    #Temporarily disable auto-registration to avoid 403s at init time:
-#   resource_provider_registrations = "none"
-# }
-
-
